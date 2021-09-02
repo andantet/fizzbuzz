cb543e451295ace1ec1c0d2a59caa90f5b0ede9d
Daniel Orr <orrdaniel73@gmail.com>
2021-09-02 23:45:37 +0100

    implement a predicate to string map system

--- src/main/java/me/andante/fizzbuzz/FizzBuzz.java
+++ src/main/java/me/andante/fizzbuzz/FizzBuzz.java
@@ -1,17 +1,25 @@
 package me.andante.fizzbuzz;
 
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
 public class FizzBuzz {
+    public static final ImmutableMap<Predicate<Integer>, String> PREDICATES =
+        new ImmutableMap.Builder<Predicate<Integer>, String>()
+            .put(i -> i % 3 == 0, "Fizz")
+            .put(i -> i % 5 == 0, "Buzz")
+        .build();
+
     public static void main(String[] args) {
         for (int i = 1; i < 100; i++) {
-            boolean fizz = i % 3 == 0;
-            boolean buzz = i % 5 == 0;
-
             StringBuilder builder = new StringBuilder();
-            if (fizz) {
-                builder.append("Fizz");
-            }
-            if (buzz) {
-                builder.append("Buzz");
+            for (Map.Entry<Predicate<Integer>, String> entry : PREDICATES.entrySet()) {
+                Predicate<Integer> p = entry.getKey();
+                if (p.test(i)) {
+                    builder.append(entry.getValue());
+                }
             }
 
             System.out.println(builder.isEmpty() ? i : builder.toString());
